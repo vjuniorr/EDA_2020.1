13ff4680c531740c9aa740a57adbae16db1320aa
vjuniorr <valdemijunior16@alu.ufc.br>
2020-03-12 11:14:07 -0300

    Implementações de AVL

--- AVL_Implementation_09_march_2020/avl.cpp
+++ AVL_Implementation_09_march_2020/avl.cpp
@@ -19,3 +19,39 @@ Tkey avl_getKey(Node *node) {
 Tvalue avl_getValue(Node *node) {
     return node->value;
 }
+
+Node *avl_search(Node *node, Tkey key){
+    if(node->key > key){
+        return avl_search(node->left, key);
+    }else if(node->key < key){
+        return avl_search(node->right, key);
+    }
+    return node;
+}
+
+void avl_preorder(Node *node){
+    if(node == nullptr){
+        return;
+    }
+    
+    cout << node->key << " : " << node->value << endl;
+    avl_preorder(node->left);
+    avl_preorder(node->right);
+}
+
+void avl_inorder(Node *node){
+    if(node == nullptr){
+        return;
+    }
+
+    avl_inorder(node->left);
+    cout << node->key << " : " << node->value << endl;
+    avl_inorder(node->right);
+}
+
+bool avl_empty(Node *node){
+    if(node == nullptr){
+        return true;
+    }
+    return false;
+}
