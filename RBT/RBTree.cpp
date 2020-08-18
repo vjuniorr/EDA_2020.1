e41705c8639a6791b2862ac377fa392d07ff9182
vjuniorr <valdemijunior16@alu.ufc.br>
2020-08-18 20:01:27 -0300

    Update

--- RBT/RBTree.cpp
+++ RBT/RBTree.cpp
@@ -37,3 +37,83 @@ void RBTree::clear (Node* node){
         }
     }
 }
+
+void RBTree::left_rotate(Node* node){
+    Node* aux = node->right; 
+    node->right = aux->left;
+
+    if(aux->left != nil){
+        aux->left->parent = node;
+    }
+    aux->parent = node->parent;
+    if(node->parent == nil){
+        root = aux;
+    }else if(node == node->parent->left){
+        node->parent->left = aux;
+    }else{
+        node->parent->right = aux;
+    }
+
+    aux->left = node;
+    node->parent = aux;
+}
+
+void RBTree::right_rotate(Node* node){
+    Node* aux = node->left;
+    node->left = aux->right;
+
+    if(aux->right != nil){
+        aux->right->parent = node;
+    }
+    aux->parent = node->parent;
+    if(node->parent == nil){
+        root = aux;
+    }else if(node == node->parent->right){
+        node->parent->right = aux;
+    }else{
+        node->parent->left = aux;
+    }
+
+    aux->right = node;
+    node->parent = aux;
+}
+
+void RBTree::insert(const Tkey &key, const Tvalue &v){
+    Node* novo = new Node{};
+    novo->key = key;
+    novo->value = v;
+    novo->left = novo->right = nil;
+    novo->parent = nil;
+
+    RBinsert(novo);
+}
+
+void RBTree::RBinsert(Node* node){
+    Node* father = nil;
+    Node* son = root;
+
+    while(son != nil){
+        father = son;
+
+        if(node->key == son->key){
+            delete node;
+            return;
+        }
+        if(node->key < son->key){
+            son = son->left;
+        }
+        son = son->right;
+    }
+    node->parent = father;
+    if(father == nil){
+        root = node;
+    }else if(node->key < father->key){
+        father->left = node;
+    }else{
+        father->right = node;
+    }
+    node->left = node->right = nil;
+    node->color = RED;
+
+    RBinsert_fixUp(node);
+}
